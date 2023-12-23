2f9e0308815f202e63f018fb7f3c07f188e57e19
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2023-12-23 11:29:21 +0530

    Create LinkedListTraversal.js

---
+++ LinkedListTraversal.js
@@ -0,0 +1,33 @@
+class MyNode {
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
+}
+
+const A = new MyNode('A');
+const B = new MyNode('B');
+const C = new MyNode('C');
+const D = new MyNode('D');
+
+A.next = B;
+B.next = C;
+C.next = D;
+
+function printTraversal(head) {
+    while(head!==null) {
+        console.log(head.value);
+        head = head.next;
+    }
+}
+
+printList(A);
+
+
+function printRecursiveWay(head){
+    if(head===null) return;
+    console.log(head.value);
+    printRecursiveWay(head.next);
+}
+
+printRecursiveWay(A);
